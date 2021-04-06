201fcb9bfed2d432ee299d3cca0fbdbf9cc2aba8
Simon <simon.cheng@advantech.com.tw>
2021-04-06 14:55:38 +0800

    Add function component example

--- simon-cheng/src/main.rs
+++ simon-cheng/src/main.rs
@@ -1,3 +1,31 @@
+use yew_services::{ConsoleService};
+use yew_functional::{function_component, use_state};
+use yew::prelude::*;
+use std::rc::Rc;
+
 fn main() {
-    println!("Hello, world!");
+    ConsoleService::info("Hello, world!");
+    App::<UseState>::new().mount_to_body();
+}
+
+#[function_component(UseState)]
+pub fn state() -> Html {
+    let (
+        counter,
+        set_counter,
+    ) = use_state(|| 0);
+    let onclick = {
+        let counter = Rc::clone(&counter);
+        Callback::from(move |_| set_counter(*counter + 1))
+    };
+
+    html! {
+        <div>
+            <button onclick=onclick>{ "Increment value" }</button>
+            <p>
+                <b>{ "Current value: " }</b>
+                { counter }
+            </p>
+        </div>
+    }
 }
